bd368ce37d2edfc2e2f9ffb75571063e2c0286a2
rian <riandiuniska@gmail.com>
2020-04-09 21:56:07 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan program
+
+    String nama = "Muhammad Riandi";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+    
+}
