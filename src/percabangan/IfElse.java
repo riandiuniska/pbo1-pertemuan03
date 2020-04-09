d497ba4af182bc99f4aa77ab71ec6707a2eb5050
rian <riandiuniska@gmail.com>
2020-04-09 22:51:58 +0800

    Percabangan IF ELSE dengan condition1 false, condition2 true. Sehingga statement2 jalan

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -3,12 +3,15 @@ package percabangan;
 public class IfElse {
     public static void main(String[] args) {
         double totalBelanja = 25000;
-        double uangDiDompet = 50000;
+        double uangDiDompet = 53000;
 
-        if (uangDiDompet<totalBelanja){
-            System.out.println("Uang Kurang, kurangi jajan Anda");
+        if (uangDiDompet<totalBelanja){  //condition 1
+            System.out.println("Uang Kurang, kurangi jajan Anda");  //statement 1
+        }else if(uangDiDompet>totalBelanja){  //condition 2
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang Cukup, angsulnya : "+angsul);  //statement 2
         }else{
-            System.out.println("Uang Cukup, Selamat menikmati");
+            System.out.println("Uang Cukup, Selamat menikmati");    //statement 3
         }
     }
 }
